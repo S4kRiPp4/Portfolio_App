b2ea9da32fe2e336dca9f8b1afd65aae0787b1f3
S4kRiPp4 <jessemb11@gmail.com>
2024-09-26 01:06:37 -0600

    added tests

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,53 @@
 require "test_helper"
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @student = Student.new(
+      first_name: "Joe",
+      last_name: "Doug",
+      school_email: "jdoug@msudenver.edu",
+      major: "Computer Science",
+      graduation_date: "2024-05-15"
+    )
+  end
+
+  test "valid student" do
+    assert @student.valid?
+  end
+
+  test "first name is required" do
+    @student.first_name = nil
+    assert_not @student.valid?
+  end
+
+  test "last name is required" do
+    @student.last_name = nil
+    assert_not @student.valid?
+  end
+
+  test "school email is required" do
+    @student.school_email = nil
+    assert_not @student.valid?
+  end
+
+  test "major is required" do
+    @student.major = nil
+    assert_not @student.valid?
+  end
+
+  test "graduation date is required" do
+    @student.graduation_date = nil
+    assert_not @student.valid?
+  end
+
+  test "school email must be unique" do
+    duplicate_student = @student.dup
+    @student.save
+    assert_not duplicate_student.valid?
+  end
+
+  test "school email must follow MSU email format" do
+    @student.school_email = "invalid_email_format"
+    assert_not @student.valid?
+  end
 end
